4b33ebbfadbc3dbde1c7cb8b76a6bcfafe0a9645
Brujitari <natimontesg2@mail.com>
2022-02-15 00:30:42 +0100

    feat: empieza la semana de JAVA

---
+++ JAVA/Día 1/java.js
@@ -0,0 +1,8 @@
+let variableSinValor
+let booleano1= Boolean
+let booleano2= Boolean
+const PI = 3.14
+const TAU = 2*PI
+
+const booleanoAnd = Boolean booleano1 === booleano2
+const booleanoNot = Boolean
