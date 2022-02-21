7639c3088566bb7d80c278e551723726cb106d26
Brujitari <natimontesg2@mail.com>
2022-02-21 18:26:26 +0100

    Started to work in second kata

---
+++ Katas/1.js
@@ -0,0 +1,16 @@
+/* const verduras = ['one', 'two', 'three']
+
+for (let i=0; i<verduras.length; i++) {
+    console.log(verduras[i])
+}
+
+ */
+
+//FizzBuzz
+for (let i=0; i<=1000 ;i++) {
+    i % 3 === 0 && i%5 === 0? return FizzBuzz
+    :
+    i % 3 === 0? return Fizz 
+    :
+    i % 5 === 0? return Buzz
+}
